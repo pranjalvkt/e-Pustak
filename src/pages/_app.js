b5f19b725036027bd05404f5c1e1f99a697c82cd
= <=>
2025-10-23 21:08:30 +0530

    scroll fix and GA added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,33 @@
 import "@/styles/globals.css";
 import { LanguageProvider } from "../context/LanguageContext";
 import Head from "next/head";
+import Script from "next/script";
+import * as gtag from "/lib/gtag";
 
 function App({ Component, pageProps }) {
   return (
-    <LanguageProvider>
-      <Head>
-        <title>e Pustak</title>
-      </Head>
-      <Component {...pageProps} />
-    </LanguageProvider>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+      <LanguageProvider>
+        <Head>
+          <title>e Pustak</title>
+        </Head>
+        <Component {...pageProps} />
+      </LanguageProvider>
+    </>
   );
 }
 export default App;
